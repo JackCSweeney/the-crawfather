53e3bcb530f16e0787dfb677bd2ddc9857f2f45c
Jack Sweeney <jackcsweeney@gmail.com>
2024-08-14 15:04:34 -0700

    feat: Adjust User model to have roundup_status attribute be an enum for enabled and disabled

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  validates :name, :email, :roundup_status, :password, presence: true
+  validates :name, :email, :password, presence: true
   validates :credits, presence: true, numericality: true
   has_many :questions, dependent: :destroy
   has_secure_password
+  enum roundup_status: ["enabled", "disabled"]
 end
