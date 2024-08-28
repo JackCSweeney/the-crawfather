4f8fd7c6c56f555f585156869f88a77b3e6a7bca
Jack Sweeney <jackcsweeney@gmail.com>
2024-08-28 11:29:17 -0700

    test: Add tests for content that should be present on home page in happy path scenario

---
+++ spec/features/users/user_show_page_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'User Show Page', type: :feature do
+  describe 'As a User' do
+    before(:each) do
+      user_params = {name: "test", email: "test@email.com", password: "password", password_confirmation: "password", roundup_status: 1}
+      @user = User.create!(user_params)
+    end
+
+    describe '#Happy Path' do
+      before(:each) do
+        visit '/'
+        fill_in 'Email', with: @user.email
+        fill_in 'Password', with: @user.password
+        click_button 'Login'
+      end
+
+      it 'has a button to go to the users dashboard' do
+        click_button 'Dashboard'
+
+        expect(current_path).to eq('/dashboard')
+      end
+
+      it 'has a button to logout' do
+        expect(page).to have_button('Log Out')
+      end
+
+      it 'has a text field to type in the question for the crawfather and a button to submit it' do
+        expect(page).to have_field('Question')
+        expect(page).to have_button('Ask the Crawfather')
+      end
+
+      it 'has the crawfather image as the background' do
+        expect(page).to have_img('shrimp.png')
+      end
+    end
+  end
+end
