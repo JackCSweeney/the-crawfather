823ef983e7f8d1da101e7c95bfab109f9e387e29
Jack Sweeney <jackcsweeney@gmail.com>
2024-09-02 13:19:48 -0700

    feat: Update conditional statement for recent_question? model method to avoid erroring on new users with no recent questions

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   enum roundup_status: ["enabled", "disabled"]
 
   def recent_question?
-    self.questions.last.created_at >= 2.minutes.ago
+    if self.questions.last
+      self.questions.last.created_at >= 2.minutes.ago
+    else
+      false
+    end
   end
 end
