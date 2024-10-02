6fb27cb78b3090bcfed3a0cc05b894f5376fa3b1
Jack Sweeney <jackcsweeney@gmail.com>
2024-10-02 14:25:05 -0700

    feat: Add get_sleeper_user_id and get_id_and_create class methods to UserSleeperAccount model

--- app/models/user_sleeper_account.rb
+++ app/models/user_sleeper_account.rb
@@ -1,4 +1,13 @@
 class UserSleeperAccount < ApplicationRecord
   belongs_to :user
   validates :sleeper_username, :sleeper_user_id, :user_id, presence: true
+
+  def self.get_sleeper_user_id(sleeper_username)
+    SleeperFacade.get_sleeper_user_id(sleeper_user_name)
+  end
+
+  def self.get_id_and_create(sleeper_username, user_id)
+    sleeper_user_id = get_sleeper_user_id(sleeper_username)
+    UserSleeperAccount.create!({user_id: user_id, sleeper_username: sleeper_username, sleeper_user_id: sleeper_user_id})
+  end
 end
