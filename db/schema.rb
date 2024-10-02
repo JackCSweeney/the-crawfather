8dc9b369e9bac77c8f6cf39d232ca60261e485a8
Jack Sweeney <jackcsweeney@gmail.com>
2024-10-02 12:27:24 -0700

    fix: Fix spelling in migration for user_sleeper_accounts

--- db/schema.rb
+++ db/schema.rb
@@ -51,13 +51,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_02_191412) do
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
-  create_table "user_sleeper_account", force: :cascade do |t|
+  create_table "user_sleeper_accounts", force: :cascade do |t|
     t.string "sleeper_username"
     t.integer "sleeper_user_id"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_user_sleeper_account_on_user_id"
+    t.index ["user_id"], name: "index_user_sleeper_accounts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -73,5 +73,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_02_191412) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "questions", "users"
-  add_foreign_key "user_sleeper_account", "users"
+  add_foreign_key "user_sleeper_accounts", "users"
 end
