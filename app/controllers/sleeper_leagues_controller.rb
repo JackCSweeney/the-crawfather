48dd1a21cfe79e90df7cdd608658f6d63eab1fbe
Jack Sweeney <jackcsweeney@gmail.com>
2024-10-09 11:19:26 -0700

    feat-in-progress: Add UserSleeperAccount and UserSleeperLeague creation process to create action in SleeperLeaguesController in order to have data to gather matchup information to display on user dashboard once a sleeperleague is added to their account

--- app/controllers/sleeper_leagues_controller.rb
+++ app/controllers/sleeper_leagues_controller.rb
@@ -1,7 +1,8 @@
 class SleeperLeaguesController < ApplicationController
   def create
-    require 'pry' ; binding.pry
+    user = User.find(session[:user_id])
     user_sleeper_account = UserSleeperAccount.get_id_and_create(session[:user_id], params[:sleeper_username])
-    # user_sleeper_league 
+    user_sleeper_league = UserSleeperLeague.get_league_and_create(session[:sleeper_league_id], session[:user_id])
+    # user_sleeper_team_matchup
   end
 end
