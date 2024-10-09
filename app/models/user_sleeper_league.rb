bcda8a867c14ac9e3ebf9b214a32b7fdf83a949c
Jack Sweeney <jackcsweeney@gmail.com>
2024-10-09 11:16:43 -0700

    feat: Add get_sleeper_league_name and get_league_and_create for UserSleeperLeague model

--- app/models/user_sleeper_league.rb
+++ app/models/user_sleeper_league.rb
@@ -1,5 +1,14 @@
 class UserSleeperLeague < ApplicationRecord
-  validates :league_name, :sleeper_league_id, presence: true
+  validates :sleeper_league_name, :sleeper_league_id, presence: true
   validates :sleeper_league_id, numericality: true
   belongs_to :user
+
+  def self.get_league_and_create(sleeper_league_id, user_id)
+    sleeper_league_name = get_sleeper_league_name(sleeper_league_id)
+    UserSleeperLeague.create!({user_id: user_id, sleeper_league_name: sleeper_league_name, sleeper_league_id: sleeper_league_id})
+  end
+
+  def self.get_sleeper_league_name(sleeper_league_id)
+    SleeperFacade.get_sleeper_league_name(sleeper_league_id)
+  end
 end
