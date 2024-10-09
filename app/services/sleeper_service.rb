afdfe10d33bc1725ec8a13fd5e04755755648e9d
Jack Sweeney <jackcsweeney@gmail.com>
2024-10-09 10:42:12 -0700

    fix: Update endpoint version for getting user sleeper account information from sleeper API

--- app/services/sleeper_service.rb
+++ app/services/sleeper_service.rb
@@ -1,10 +1,10 @@
 class SleeperService
   def self.get_sleeper_user_id(sleeper_username)
-    response = conn.get("/user/#{sleeper_username}")
+    response = conn.get("/v1/user/#{sleeper_username}")
     JSON.parse(response.body, symbolize_names: true)[:user_id].to_i
   end
 
   def self.conn
-    Faraday.new(url: "https://api.sleeper.app/v1")
+    Faraday.new(url: "https://api.sleeper.app")
   end
 end
