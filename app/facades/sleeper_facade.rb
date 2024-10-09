e2af2e697a7719b3b1f1abdeeef260d08003696d
Jack Sweeney <jackcsweeney@gmail.com>
2024-10-09 11:12:25 -0700

    feat: Add get_sleeper_league_name(sleeper_league_id) to SleeperFacade to retrieve sleeper league name from SleeperService

--- app/facades/sleeper_facade.rb
+++ app/facades/sleeper_facade.rb
@@ -2,4 +2,8 @@ class SleeperFacade
   def self.get_sleeper_user_id(sleeper_username)
     SleeperService.get_sleeper_user_id(sleeper_username)
   end
+
+  def self.get_sleeper_league_name(sleeper_league_id)
+    SleeperService.get_sleeper_league_name(sleeper_league_id)
+  end
 end
